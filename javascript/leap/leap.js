e1d78070acd4faeec0d50b9453a84bd600d0e765
Christian Lazo <lazochristian.cl@gmail.com>
2018-11-04 18:37:47 -0800

    JS: solution for leap year

---
+++ javascript/leap/leap.js
@@ -0,0 +1,8 @@
+export const isLeap = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 !== 0 || year % 400 == 0) {
+             return true;
+        }
+    }
+    return false;
+}
